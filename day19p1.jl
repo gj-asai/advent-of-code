6cd6bf68cf6186f04b7db9bc400cd3c22e7312e8
Gustavo Asai <gustavojun.asai@gmail.com>
2024-12-19 15:31:07 +0100

    day 19

---
+++ day19p1.jl
@@ -0,0 +1,26 @@
+filename = "day19/input.txt"
+# filename = "day19/test.txt"
+
+towels = ""
+designs = Vector{String}()
+open(filename, "r") do f
+    global towels = split(readline(f), ", ")
+    readline(f)
+
+    while !eof(f)
+        push!(designs, readline(f))
+    end
+end
+
+function is_possible(design, towels)
+    length(design) == 0 && return true
+    for t in towels
+        length(t) <= length(design) || continue
+        t == view(design, 1:length(t)) || continue
+        is_possible(design[length(t)+1:end], towels) || continue
+        return true
+    end
+    return false
+end
+
+[is_possible(d, towels) for d in designs] |> sum
